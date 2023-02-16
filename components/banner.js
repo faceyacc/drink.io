a2f9a172605443e01c9ffdfb90b72fdc84993bfb
Ty <justfacey@gmail.com>
2023-02-16 00:36:05 -0800

    made searc button look snazzy

--- components/banner.js
+++ components/banner.js
@@ -3,17 +3,19 @@ import { Inter } from '@next/font/google'
 import coffeeMovie from '../public/static/coffee.mp4'
 
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-const Banner = () => {
+const Banner = (props) => {
+
+    const handleOnBannerBtnClick = () => {}
 
     return (
         <>
             {/* <video src={coffeeMovie} className={styles.titleimage} autoPlay muted loop/> */}
             <div className={styles.container}>
-                <h1 className={styles.bottomleft}> Drink.io </h1>
-                <p>Discover you local coffee shops!</p>
-                <button className={styles.button}>View shops</button>
+                <h1 className={styles.bottomleft}> Drink<span className={styles.color}>.</span>io </h1>
+                <p>Discover your local coffee shops!</p>
+                <button className={styles.button} onClick={props.handleOnClick}> {props.buttonText} </button>
             </div>
             
             <video src={coffeeMovie} className={styles.titleimage} autoPlay muted loop/>
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Sidebar from '../components/sidebar'
 export default function App({ Component, pageProps }) {
   return (
   <>
-    <Sidebar/>
+    {/* <Sidebar/> */}
     <Component {...pageProps} />
   </> 
 )
