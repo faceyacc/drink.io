7f7c5f231f5ae357de899a4c0ef955d1ea726a8b
Ty <justfacey@gmail.com>
2023-02-20 15:46:30 -0800

    added global fonts

--- components/banner.js
+++ components/banner.js
@@ -1,21 +1,16 @@
 import styles from './Banner.module.css'
-import { Inter } from '@next/font/google'
 import coffeeMovie from '../public/static/coffee.mp4'
-
-
-// const inter = Inter({ subsets: ['latin'] })
 
 const Banner = (props) => {
 
     return (
         <>
-            {/* <video src={coffeeMovie} className={styles.titleimage} autoPlay muted loop/> */}
+            <video src={coffeeMovie} className={styles.titleimage} autoPlay muted loop/>
             <div className={styles.container}>
-                <h1 className={styles.bottomleft}> Drink<span className={styles.color}>.</span>io </h1>
+                <h1 className> Drink<span className={styles.color}>.</span>io</h1>
                 <p>Discover your local coffee shops!</p>
                 <button  onClick={props.handleOnClick}> {props.buttonText} </button>
-            </div>            
-            <video src={coffeeMovie} className={styles.titleimage} autoPlay muted loop/>
+            </div>  
             
         </>
     )
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,45 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head/>
+
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=BioRhyme:wght@300&family=Gloock&display=swap"
+            rel="stylesheet"
+          />   
+
+          <link
+              rel="preload"
+              href="/fonts/BioRhyme-Bold.ttf"
+              as="font"
+              crossOrigin="anonymous"
+            ></link>
+
+          <link
+              rel="preload"
+              href="./public/fonts/BioRhyme-Light.ttf"
+              as="font"
+              crossOrigin="anonymous"
+            ></link>   
+
+          <link
+            rel="preload"
+            href="./public/fonts/BioRhyme-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>       
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
